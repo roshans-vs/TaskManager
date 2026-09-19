60f277eff8e26d3eb7f922ac872f18904de1ef99
roshan <sroshan27@gmail.com>
2026-09-19 12:34:47 +0530

    Initial commit

---
+++ src/TaskManager.java
@@ -0,0 +1,11 @@
+public class TaskManager {
+
+    public static void main(String[] args) {
+
+        System.out.println("Task Manager");
+        System.out.println("------------");
+        System.out.println("1. Add Task");
+        System.out.println("2. View Tasks");
+        System.out.println("3. Exit");
+    }
+}
